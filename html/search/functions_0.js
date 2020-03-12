9eb09af37e46efa5550bc2fb216236616e7d7ba9
Doxygen CI <travis@travis-ci.org>
2020-03-12 17:44:11 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5fbegin',['_begin',['../class_adafruit___neo_pixel___zero_d_m_a.html#a0ae6b17aa22f87469cf3550c5cde129f',1,'Adafruit_NeoPixel_ZeroDMA']]]
+  ['_5fbegin',['_begin',['../class_adafruit___neo_pixel___zero_d_m_a.html#a242549d8bb6bce2b265641053703c2a9',1,'Adafruit_NeoPixel_ZeroDMA']]]
 ];
